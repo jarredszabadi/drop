945ebdfbbe874e753765c2f11c35513db1a715e7
Jarred Szabadi <sazbatz@gmail.com>
2013-07-09 18:10:28 -0400

    location services enabled, press drop to get current lat, long coordiantes....testing for gps without wifi

--- src/com/example/dropchatv2/MainActivity.java
+++ src/com/example/dropchatv2/MainActivity.java
@@ -1,25 +1,149 @@
 package com.example.dropchatv2;
 
+
+
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+
+import android.location.LocationListener;
+
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 
-public class MainActivity extends Activity {
 
+
+public class MainActivity extends FragmentActivity implements LocationListener {
+	GoogleMap map;
+	private LocationManager locationManager;
+	private String provider;
+	double lat;
+	double lon;
+	Location loc;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
 		
-	}
+		SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+		map = fm.getMap();
+		Button drop = (Button) findViewById(R.id.drop);
+		
+		
+		map.setMyLocationEnabled(true);
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	    // Define the criteria how to select the locatioin provider -> use
+	    // default
+	    Criteria criteria = new Criteria();
+	    provider = locationManager.getBestProvider(criteria, false);
+	    loc = locationManager.getLastKnownLocation(provider);
+		
+	    if(loc!=null){
+            onLocationChanged(loc);
+        }
+        locationManager.requestLocationUpdates(provider, 20000, 0, this);
+	    
+	    drop.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				EditText location = (EditText) findViewById(R.id.header);
+				location.setText(lat+","+lon);
 
+			}
+
+		});
+		 
+		
+		
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	/* Request updates at startup */
+	  @Override
+	  protected void onResume() {
+	    super.onResume();
+	    locationManager.requestLocationUpdates(provider, 400, 1, this);
+	  }
+
+	  /* Remove the locationlistener updates when Activity is paused */
+	  @Override
+	  protected void onPause() {
+	    super.onPause();
+	    locationManager.removeUpdates(this);
+	  }
+
+	  @Override
+	  public void onLocationChanged(Location location) {
+	    lat = (double) (location.getLatitude());
+	    lon = (double) (location.getLongitude());
+	    loc = location;
+	    
+	    
+	    
+	    LatLng latLng = new LatLng(lat, lon);
+	    
+        // Showing the current location in Google Map
+        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+ 
+        // Zoom in the Google Map
+        map.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+        
+	    
+	  }
+
+
+
+
+	  @Override
+	  public void onProviderDisabled(String provider) {
+	    Toast.makeText(this, "Disabled provider " + provider,
+	        Toast.LENGTH_SHORT).show();
+	  }
+
+	@Override
+	public void onProviderEnabled(String provider) {
+	    Toast.makeText(this, "Enabled new provider " + provider,
+	        Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	 public void onStatusChanged(String provider, int status, Bundle extras) {
+	    // TODO Auto-generated method stub
+
+	 }
+	
+	
+
+    
+    
 
 }
